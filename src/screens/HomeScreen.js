7cc4b2e5b4ba556fdde08570bd82fe17489548e8
eric <eaize9@gmail.com>
2022-03-26 19:09:34 -0500

    init

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from '../components/Header'
+
+export function HomeScreen() {
+
+    return (
+        <div>
+            <Header />
+            <Container>
+            </Container>
+        </div>
+    )
+}
+
+export default HomeScreen
