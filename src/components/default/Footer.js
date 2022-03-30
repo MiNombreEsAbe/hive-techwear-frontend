cbf210ade621c3228c582da47efc501ad1c894e6
morenoa2 <morenoa2@berea.edu>
2022-03-30 15:56:25 -0400

    Modified Footer and Footer css

--- src/components/default/Footer.js
+++ src/components/default/Footer.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { ContactUs } from '../contactUs/ContactUs';
+// import { ContactUs } from '../contactUs/ContactUs';
 import { SocialIcon } from 'react-social-icons';
+
 export function Footer () {
   return (
     <footer className="footer">
@@ -9,18 +10,24 @@ export function Footer () {
         <div className="footer-container">
           <h2>Shop and Learn</h2>
           <div className="footer-content">
-          <ContactUs />
-            <Link to="/sign-up">Sign up</Link>
-            <Link to="/sign-in">Sign in</Link>
-            <Link to="/">Home</Link>
+            {/* <ContactUs /> */}
+            <Link to="/signup">Sign up</Link>
+            <Link to="/signin">Sign in</Link>
+            <Link to="/itemlists">Item Lists</Link>
           </div>
-          <div className="social">
-          <SocialIcon url="https://twitter.com/" />
-          <SocialIcon url="https://facebook.com/" />
+        </div>
+        <div className="footer-container">
+          <h2>Support</h2>
+          <div className="footer-content">
+            {/* <ContactUs /> */}
+            <Link to="/contactus">Contact Us</Link>
           </div>
         </div>
       </div>
-      <hr className="line" />
+      <div className="social">
+        <SocialIcon className="twitter" url="https://twitter.com/" />
+        <SocialIcon url="https://facebook.com/" />
+      </div>
       <p className="copyright">
         Copyright © 2022 HIVE TechWear. All rights reserved. <Link to="/policy">Privacy Policy</Link> | <Link to='/terms'>Term
         of Use Sales Policy</Link> | <Link to='/legal'>Legal</Link> | <Link to='/xml'>Site Map</Link>
