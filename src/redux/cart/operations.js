13add53082429d3161438b2e638ee5fd9b40b55c
morenoa2 <morenoa2@berea.edu>
2022-04-12 18:25:38 -0400

    Changed API.js

--- src/redux/cart/operations.js
+++ src/redux/cart/operations.js
@@ -1,5 +1,21 @@
 // This file contains the operations for Cart
 import { addItemAction, removeItemAction } from './actions';
+import API from '../../API';
+
+const api = new API();
+
+export const getCart = () => {
+    return async dispatch => {
+        return await api
+            .getCart()
+            .then(res => {
+                console.log(res)
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+}
 
 export const addItem = item => async dispatch => dispatch(addItemAction(item));
 export const removeItem = item => async dispatch => dispatch(removeItemAction(item));
